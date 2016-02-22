7f5563bc4e2c26425cd8a099747058a56aa89b46
Manfred Wolff <m.wolff@neusta.de>
2016-02-22 14:45:01 +0100

    Solving some minor issues with java doc.

--- src/main/java/de/mwolff/commons/command/iface/CommandContainer.java
+++ src/main/java/de/mwolff/commons/command/iface/CommandContainer.java
@@ -32,8 +32,8 @@ public interface CommandContainer<T extends Context> extends Command<T>, ChainCo
 
     /**
      * Adds a <code>Command</code> to the list. Because a
-     * <code>CommandList</code> is a <code>Command</code> you can add
-     * <code>CommandList</code> objects as well.
+     * <code>CommandContainer</code> is a <code>Command</code> you can add
+     * <code>CommandContainer</code> objects as well.
      *
      * @param command
      */
@@ -41,8 +41,8 @@ public interface CommandContainer<T extends Context> extends Command<T>, ChainCo
 
     /**
      * Adds a <code>Command</code> to the list via priority. Because a
-     * <code>CommandList</code> is a <code>Command</code> you can add
-     * <code>CommandList</code> objects as well.
+     * <code>CommandContainer</code> is a <code>Command</code> you can add
+     * <code>CommandContainer</code> objects as well.
      *
      * @param priority
      * @param command
