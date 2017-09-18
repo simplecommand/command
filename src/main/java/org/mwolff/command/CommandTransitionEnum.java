4cade8abd895a39a9ec2d61b842df53844daee37
Manfred Wolff <wolff@manfred-wolff.de>
2017-09-18 14:09:13 +0200

    Added NEXT to the Transitions.

--- src/main/java/org/mwolff/command/CommandTransitionEnum.java
+++ src/main/java/org/mwolff/command/CommandTransitionEnum.java
@@ -1,20 +1,47 @@
+/**
+    Simple Command Framework.
+
+    Framework for easy building software that fits the SOLID principles.
+    @author Manfred Wolff <m.wolff@neusta.de>
+
+    Download: https://mwolff.info:7990/bitbucket/scm/scf/simplecommandframework.git
+
+    Copyright (C) 2018 Manfred Wolff and the simple command community
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+ */
 package org.mwolff.command;
 
 public interface CommandTransitionEnum {
 
     /**
+     * @since 1.5.0
      * Transition for an execute Method.
      *
      * State can be:
      *
-     * SUCCESS : Execution was Successfully done. The next command can overtake.
+     * SUCCESS : Execution was Successfully done. Used for Command-Mode
      * DONE    : For CoR: The work is successfully done, the chain can be aborted.
+     * NEXT    : For CoR: The work is not done, next chain should overtake.
      * FAILURE : An fatal error has occurred. The chain should be aborted
      *
      * @since 1.5.0
      */
     public enum CommandTransition {
-        SUCCESS, FAILURE, DONE
+        SUCCESS, FAILURE, NEXT, DONE
     }
 
 }
