b5522a9857b0888fe50595cf6d0b2091f2adcb50
Manfred Wolff <wolff@manfred-wolff.de>
2017-09-19 15:47:21 +0200

    Extending a default handler

--- src/main/java/org/mwolff/command/sax/ActionContentHandler.java
+++ src/main/java/org/mwolff/command/sax/ActionContentHandler.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 import org.mwolff.command.process.DefaultTransition;
 import org.mwolff.command.process.Transition;
 import org.xml.sax.Attributes;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-public class ActionContentHandler implements ContentHandler {
+public class ActionContentHandler extends DefaultHandler {
 
     private ArrayList<Action> actions = new ArrayList<>();
 
@@ -46,32 +45,5 @@ public class ActionContentHandler implements ContentHandler {
         if (localName.equals("action")) {
             actions.add(action);
         }
-    }
-
-    public void endDocument() throws SAXException {
-    }
-
-    public void endPrefixMapping(String prefix) throws SAXException {
-    }
-
-
-    public void setDocumentLocator(Locator locator) {
-    }
-
-    public void skippedEntity(String name) throws SAXException {
-    }
-
-    public void startDocument() throws SAXException {
-    }
-
-    public void startPrefixMapping(String prefix, String uri) throws SAXException {
-    }
-
-    @Override
-    public void ignorableWhitespace(char[] arg0, int arg1, int arg2) throws SAXException {
-    }
-
-    @Override
-    public void processingInstruction(String arg0, String arg1) throws SAXException {
     }
 }
