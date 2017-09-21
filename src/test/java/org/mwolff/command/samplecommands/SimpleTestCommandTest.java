0f1121a32d80c5861496e5d25e33633c9fdf7f30
Michael Albrecht <malbrecht@laptopc3104>
2017-09-21 16:23:28 +0200

    deleted redundant rule usage

--- src/test/java/org/mwolff/command/samplecommands/SimpleTestCommandTest.java
+++ src/test/java/org/mwolff/command/samplecommands/SimpleTestCommandTest.java
@@ -2,24 +2,19 @@ package org.mwolff.command.samplecommands;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 import org.mwolff.command.Command;
 import org.mwolff.command.parameterobject.DefaultParameterObject;
 import org.mwolff.command.parameterobject.GenericParameterObject;
 
 public class SimpleTestCommandTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test
-    public void testExecuteCommand() throws Exception {
-        final GenericParameterObject context = new DefaultParameterObject();
-        final Command<GenericParameterObject> command = new SimpleTestCommand<>();
-        command.executeCommand(context);
-        Assert.assertThat(context.getAsString("SimpleTestCommand"), CoreMatchers.is("SimpleTestCommand"));
-        Assert.assertThat(context.getAsString("priority"), CoreMatchers.is("S-"));
-    }
+   @Test
+   public void testExecuteCommand() throws Exception {
+      final GenericParameterObject context = new DefaultParameterObject();
+      final Command<GenericParameterObject> command = new SimpleTestCommand<>();
+      command.executeCommand(context);
+      Assert.assertThat(context.getAsString("SimpleTestCommand"), CoreMatchers.is("SimpleTestCommand"));
+      Assert.assertThat(context.getAsString("priority"), CoreMatchers.is("S-"));
+   }
 }
